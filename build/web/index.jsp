1de8dcc4c95fb6bcb45c3285d0c9c20d09ac6a1a
Vagner Antunes <vagner.antunes0701@outlook.com>
2021-11-17 10:30:48 -0300

    metodos em DAOProfessor inseridos

--- build/web/index.jsp
+++ build/web/index.jsp
@@ -2,19 +2,23 @@
 <!DOCTYPE html>
 <html>
     <head>
+        
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>HOME</title>
         
     </head>
     <body>
+        
         <div>
             <h2>CADASTRO DE PROFESSOR</h2>
             <h3><a href="novo_professor.jsp"><input type="submit" value="INSERIR"/></a></h3>
+            <h3><a href="consultar_professor.jsp"><input type="submit" value="CONSULTAR"/></a></h3>
         </div>
         
         <div>
             <h2>CADASTRO DE DISCIPLINA</h2>
             <h3><a href="nova_disciplina.jsp"><input type="submit" value="INSERIR"/></a></h3>
+            <h3><a href="consultar_disciplina.jsp"><input type="submit" value="CONSULTAR"/></a></h3>
         </div>
     </body>
 </html>
--- src/java/DAO/DAOProfessor.java
+++ src/java/DAO/DAOProfessor.java
@@ -19,6 +19,7 @@ public class DAOProfessor {
         conn = new Conexao().getConexao();
     }
     
+    //inserir professor
     public void inserirProfessor(Professor professor){
         String sql = "INSERT INTO tb_professor (nome_professor,email_professor,senha_professor) VALUES (?,?,?)";
                 
@@ -31,6 +32,80 @@ public class DAOProfessor {
             stmt.close();
         } catch (Exception erro) {
             throw new RuntimeException("Erro ao inserir professor" + erro);
+        }
+    }
+    
+    //metodo listar todos professores
+    public ArrayList<Professor> listarProfessores(){
+        String sql = "SELECT * FROM tb_professor";
+        
+        try {
+            st = conn.createStatement();
+            rs = st.executeQuery(sql);
+            
+            while(rs.next()){
+                Professor professor = new Professor();
+                professor.setId(rs.getInt("id_professor"));
+                professor.setNome(rs.getString("nome_professor"));
+                professor.setEmail(rs.getString("email_professor"));
+                professor.setSenha(rs.getString("senha_professor"));
+                lista.add(professor);
+            }
+            
+        } catch (Exception erro) {
+            throw new RuntimeException("Erro no metodo listar todos professores" + erro);
+        }
+        return lista;
+    }
+    
+    //metodo buscar professor por nome
+    public ArrayList<Professor> listarProfessorNome(String valor){
+        String sql = "SELECT * FROM tb_professor WHERE nome_professor LIKE '%" + valor + "%' ";
+        
+        try {
+            st = conn.createStatement();
+            rs = st.executeQuery(sql);
+            
+            while (rs.next()) {                
+                Professor professor = new Professor();
+                professor.setId(rs.getInt("id_professor"));
+                professor.setNome(rs.getString("nome_professor"));
+                professor.setEmail(rs.getString("email_professor"));
+                professor.setSenha(rs.getString("senha_professor"));
+                lista.add(professor);
+            }
+        } catch (Exception erro) {
+            throw new RuntimeException("Erro no metodo listar professor por nome" + erro);
+        }
+        return lista;
+    }
+    
+    //metodo atualizar professor
+    public void atualizarProfessor(Professor professor){
+        try {
+            String sql = "UPDATE tb_professor SET nome_professor = ?, email_professor = ?, senha_professor = ? "
+                    + "WHERE id_professor = ?";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, professor.getNome());
+            stmt.setString(2, professor.getEmail());
+            stmt.setString(3, professor.getSenha());
+            stmt.setInt(4, professor.getId());
+            stmt.execute();
+            stmt.close();
+        } catch (Exception erro) {
+            throw new RuntimeException("Erro metodo atualizar professor");
+        }
+    }
+    
+    //metodo excluir professor
+    public void excluirProfessor(int valor){
+        try {
+            String sql = "DELETE ROM tb_professor WHERE id_professor = " + valor;
+            st = conn.createStatement();
+            st.execute(sql);
+            st.close();
+        } catch (Exception erro) {
+            throw new RuntimeException("Erro metodo excluir professor" + erro);
         }
     }
 }
